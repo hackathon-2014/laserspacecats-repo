942f930d91474454bc16c021ade533e94956a6c2
Kevin Kolz <kevinkolz@Kevin-Kolz.local>
2014-08-23 11:23:26 -0400

    Server starts

--- toot-node/server.js
+++ toot-node/server.js
@@ -4,22 +4,22 @@ var path = require('path');
 var util = require('util');
 var mongoose = require('mongoose');
 
+var models = require('./models');
 var restify = require('restify');
 
 
 ///--- Errors
-
-function badMessage() {
+function MissingDestinationError() {
     restify.RestError.call(this, {
         statusCode: 409,
-        restCode: 'BadMessage',
-        message: 'your message is bad',
-        constructorOpt: BadMessageError
+        restCode: 'MissingDestination',
+        message: 'you need a destination',
+        constructorOpt: MissingDestinationError
     });
 
-    this.name = 'BadMessageError';
+    this.name = 'MissingDestinationError';
 }
-util.inherits(BadMessageError, restify.RestError);
+util.inherits(MissingDestinationError, restify.RestError);
 
 
 ///--- Formatters
@@ -29,7 +29,7 @@ util.inherits(BadMessageError, restify.RestError);
  * demonstrate how to support additional content-types.  Really this is
  * the same as text/plain, where we pick out 'task' if available
  */
-function formatTodo(req, res, body) {
+function formatToot(req, res, body) {
     if (body instanceof Error) {
         res.statusCode = body.statusCode || 500;
         body = body.message;
@@ -79,180 +79,86 @@ function authenticate(req, res, next) {
 }
 
 
+///--- API
 /**
  * Note this handler looks in `req.params`, which means we can load request
  * parameters in a "mixed" way, like:
  *
- * POST /todo?name=foo HTTP/1.1
+ * POST /toot?name=foo HTTP/1.1
  * Host: localhost
  * Content-Type: application/json
  * Content-Length: ...
  *
- * {"task": "get milk"}
+ * {"destination": "Whitney"}
  *
- * Which would have `name` and `task` available in req.params
+ * Which would have `destination` and `origin` available in req.params
  */
-function createToot(req, res, next) {
-    if (!req.params.task) {
-        req.log.warn({params: p}, 'createTodo: missing task');
-        next(new MissingTaskError());
+function sendToot(req, res, next) {
+    if (!req.params.destination) {
+        req.log.warn({params: p}, 'createToot: missing destination');
+        next(new MissingDestination());
         return;
     }
 
-    var toot = {
-        name: req.params.name || req.params.task.replace(/\W+/g, '_'),
-        task: req.params.task
-    };
+    var toot = new Toot(
+        { 
+            origin: req.params.origin,
+            destination: req.params.destination,
+            classification: 'arrival',
+            eta: req.params.eta
+        }
+    );
 
-    if (req.todos.indexOf(todo.name) !== -1) {
-        req.log.warn('%s already exists', todo.name);
-        next(new TodoExistsError(todo.name));
+    toot.save(function (err, fluffy) {
+        if (err) return console.error(err);
+    }); 
+}
+
+function sendOTW(req, res, next) {
+    if (!req.params.destination) {
+        req.log.warn({params: p}, 'createToot: missing destination');
+        next(new MissingDestination());
         return;
     }
 
-    var p = path.normalize(req.dir + '/' + todo.name);
-    fs.writeFile(p, JSON.stringify(todo), function (err) {
-        if (err) {
-            req.log.warn(err, 'createTodo: unable to save');
-            next(err);
-        } else {
-            req.log.debug({todo: todo}, 'createTodo: done');
-            res.send(201, todo);
-            next();
+    var toot = new models.Toot(
+        { 
+            origin: req.params.origin,
+            destination: req.params.destination,
+            classification: 'otw',
+            eta: req.params.eta
         }
-    });
+    );
+
+    toot.save(function (err, fluffy) {
+        if (err) return console.error(err);
+    }); 
 }
 
+function getUser(req, res, next) {
 
-/**
- * Deletes a TODO by name
- */
-function deleteTodo(req, res, next) {
-    fs.unlink(req.todo, function (err) {
-        if (err) {
-            req.log.warn(err,
-                'deleteTodo: unable to unlink %s',
-                req.todo);
-            next(err);
-        } else {
-            res.send(204);
-            next();
-        }
-    });
 }
 
-
-/**
- * Deletes all TODOs (in parallel)
- */
-function deleteAll(req, res, next) {
-    var done = 0;
-
-    // Note this is safe, as restify ensures "next" is called
-    // only once
-    function cb(err) {
-        if (err) {
-            req.log.warn(err, 'unable to delete a TODO');
-            next(err);
-        } else if (++done === req.todos.length) {
-            next();
-        }
-    }
-
-    if (req.todos.length === 0) {
-        next();
-        return;
-    }
-
-    req.todos.forEach(function (t) {
-        var p = req.dir + '/' + t;
-        fs.unlink(p, cb);
-    });
+function createUser(req, res, next) {
+    
 }
 
-
-/**
- * Loads a TODO by name
- *
- * Requires `loadTodos` to have run.
- *
- * Note this function uses streaming, as that seems to come up a lot
- * on the mailing list and issue board.  restify lets you use the HTTP
- * objects as they are in "raw" node.
- *
- * Note by using the "raw" node APIs, you'll need to handle content
- * negotiation yourself.
- *
- */
-function getTodo(req, res, next) {
-    if (req.accepts('json')) {
-        var fstream = fs.createReadStream(req.todo);
-
-        fstream.once('open', function onOpen() {
-            res.setHeader('Content-Type', 'application/json');
-            res.writeHead(200);
-            fstream.pipe(res);
-            fstream.once('end', next);
-        });
-
-        // Really, you'd want to disambiguate the error code
-        // from 'err' here to return 403, 404, etc., but this
-        // is just a demo, so you get a 500
-        fstream.once('error', next);
-        return;
-    }
-
-    fs.readFile(req.todo, 'utf8', function (err, data) {
-        if (err) {
-            req.log.warn(err, 'get: unable to read %s', req.todo);
-            next(err);
-            return;
-        }
-
-        res.send(200, JSON.parse(data));
-        next();
-    });
+function updateUser(req, res, next) {
+    
 }
 
-
-/**
- * Replaces a TODO completely
- */
-function putTodo(req, res, next) {
-    if (!req.params.task) {
-        req.log.warn({params: req.params}, 'putTodo: missing task');
-        next(new MissingTaskError());
-        return;
-    }
-
-    // Force the name to be what we sent this to
-    var todo = {
-        name: req.params.name,
-        task: req.params.task
-    };
-
-    fs.writeFile(req.todo, JSON.stringify(req.body), function (err) {
-        if (err) {
-            req.log.warn(err, 'putTodo: unable to save');
-            next(err);
-        } else {
-            req.log.debug({todo: req.body}, 'putTodo: done');
-            res.send(204);
-            next();
-        }
-    });
+function deleteUser(req, res, next) {
+    
 }
 
-
-///--- API
+function login(req, res, next) {
+    
+}
 
 /**
  * Returns a server with all routes defined on it
  */
 function createServer(options) {
-    assert.object(options, 'options');
-    assert.string(options.directory, 'options.directory');
-    assert.object(options.log, 'options.log');
 
     // Create a server with our logger and custom formatter
     // Note that 'version' means all routes will default to
@@ -328,24 +234,12 @@ function createServer(options) {
             'POST    /message/toot',
             'GET     /user',
             'PUT     /user',
-            'POST    /user'
+            'POST    /user',
             'POST    /login'
         ];
         res.send(200, routes);
         next();
     });
-
-    // Setup an audit logger
-    if (!options.noAudit) {
-        server.on('after', restify.auditLogger({
-            body: true,
-            log: bunyan.createLogger({
-                level: 'info',
-                name: 'todoapp-audit',
-                stream: process.stdout
-            })
-        }));
-    }
 
     return (server);
 }
