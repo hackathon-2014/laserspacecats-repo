2290dd907258e1d094056e9616de86b6956f440a
Ryan Moore <rmoore4146@gmail.com>
2014-08-23 14:52:29 -0400

    user stuff

--- toot-android/app/src/main/java/cats/space/laser/toot_android/model/ApiBase.java
+++ toot-android/app/src/main/java/cats/space/laser/toot_android/model/ApiBase.java
@@ -6,14 +6,4 @@ import java.io.Serializable;
  * Created by Whitney Champion on 8/23/14.
  */
 public class ApiBase implements Serializable{
-
-    private Boolean success;
-
-    public Boolean getSuccess() {
-        return success;
-    }
-
-    public void setSuccess(Boolean success) {
-        this.success = success;
-    }
 }
