0dda1820b86020a72c8a4f7cbbd67611d09fda0c
Ryan Moore <rmoore4146@gmail.com>
2014-08-23 10:16:17 -0400

    android initial commit

---
+++ toot-android/app/src/main/java/cats/space/laser/toot_android/SplashActivity.java
@@ -0,0 +1,31 @@
+package cats.space.laser.toot_android;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+
+public class SplashActivity extends Activity {
+
+    private static final int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+
+
+}
