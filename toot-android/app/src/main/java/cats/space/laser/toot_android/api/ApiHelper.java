0d979fe6fd3003a45a3b2764eb5322465baf09d2
shortstack <whitney.ellis.champion@gmail.com>
2014-08-23 13:21:56 -0400

    api fixes

--- toot-android/app/src/main/java/cats/space/laser/toot_android/api/ApiHelper.java
+++ toot-android/app/src/main/java/cats/space/laser/toot_android/api/ApiHelper.java
@@ -48,9 +48,10 @@ public class ApiHelper {
 
     // API URL
     private static int timeout = 10000;
-    public static final String GET_USER = "/v1/user";
-    public static final String PUT_TOOT = "/v1/toot";
-    public static final String PUT_USER = "/v1/user";
+    public static final String GET_USER = "/user";
+    public static final String CREATE_USER = "/user";
+    public static final String UPDATE_USER = "/user";
+    public static final String SEND_TOOT = "/message";
 
 
     public static void get(String url, Context context, Type type,
